26ff60f24774dcf3e1f3b0d8304fc22f012e1c65
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2023-10-10 03:28:04 +0530

    create database if it is not exist when running the application

--- db.ts
+++ db.ts
@@ -1,21 +1,48 @@
-import pg from 'pg'; // Import the entire 'pg' package as default
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new pg.Pool({ // Use pg.Pool
+const pool = new pg.Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  database: 'postgres', // Use the default 'postgres' database for this query
   password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT), // Ensure port is parsed as a number
+  port: Number(process.env.DB_PORT),
 });
 
 pool.on('connect', () => {
   console.log('Connected to the database');
 });
 
-// Check if the 'todos' table exists, and create it if not
+// Check if the database exists and create it if not
+pool.query(
+  `SELECT 1 FROM pg_database WHERE datname = $1`,
+  [process.env.DB_NAME],
+  (err, res) => {
+    if (err) {
+      console.error('Error checking database existence:', err);
+    } else {
+      if (res.rows.length === 0) {
+        // The database doesn't exist, create it
+        pool.query(
+          `CREATE DATABASE ${process.env.DB_NAME}`,
+          (err, res) => {
+            if (err) {
+              console.error('Error creating database:', err);
+            } else {
+              console.log(`Database "${process.env.DB_NAME}" created`);
+            }
+          }
+        );
+      } else {
+        console.log(`Database "${process.env.DB_NAME}" already exists`);
+      }
+    }
+  }
+);
+
+// Create the 'todos' table
 pool.query(
   `CREATE TABLE IF NOT EXISTS todos (
     id SERIAL PRIMARY KEY,
--- index.ts
+++ index.ts
@@ -18,19 +18,7 @@ import todoRoutes from './routes/todoRoutes.ts';
 // Use your routes
 app.use('/todos', todoRoutes);
 
-// Get all todos
-app.get('/todos', async (req, res) => {
-  try {
-    const client = await database.connect();
-    const result = await client.query('SELECT * FROM todos');
-    client.release(); // Release the client back to the pool
 
-    res.json(result.rows);
-  } catch (err) {
-    console.error('Error fetching todos:', err);
-    res.status(500).json({ error: 'Error fetching todos' });
-  }
-});
 
 // Start the server
 app.listen(port, () => {
